cda302040d1ad1595505a7efbff9954bbb4a0af8
tastekim <tastekim@kakao.com>
2023-02-23 18:47:13 +0900

    :rocket: Check getAllDiseasesData.ts

--- getAllDiseasesData.ts
+++ getAllDiseasesData.ts
@@ -18,22 +18,6 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
                 body : JSON.stringify({message: 'Invalid query string.'})
             }
         }
-        // if (comp === 'F') {
-        //     return {
-        //         statusCode : 200,
-        //         body : JSON.stringify(movies)
-        //     };
-        // } else if (comp === 'T' || typeof comp === 'undefined') {
-        //     return {
-        //         statusCode : 200,
-        //         body : JSON.stringify(compress(movies))
-        //     };
-        // } else {
-        //     return {
-        //         statusCode : 404,
-        //         body : 'Invalid query string.'
-        //     };
-        // }
     } catch (err) {
         console.error(err);
         return {
