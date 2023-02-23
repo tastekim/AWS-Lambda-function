5741e2fde76d5a882c1edb21dc2f4b810503d760
tastekim <tastekim@kakao.com>
2023-02-23 18:39:00 +0900

    :pushpin: Check running on AWS lambda.

--- getAllDiseasesData.ts
+++ getAllDiseasesData.ts
@@ -3,20 +3,42 @@ import { compress } from 'compress-json';
 import { connectToDatabase } from './db-connect';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    const db = await connectToDatabase();
-    const movies = await db.collection('diseases').find({}).toArray();
-    const comp: unknown = event.queryStringParameters;
-
-    if (typeof comp === 'undefined') {
+    try {
+        const db = await connectToDatabase();
+        const movies = await db.collection('diseases').find({}).toArray();
+        const { comp } = event?.queryStringParameters || {};
+        if (comp === 'F' || comp === 'T' || typeof comp === 'undefined') {
+            return {
+                statusCode : 200,
+                body : JSON.stringify(comp ==='F' ? movies : compress(movies))
+            }
+        } else {
+            return {
+                statusCode : 404,
+                body : JSON.stringify({message: 'Invalid query string.'})
+            }
+        }
+        // if (comp === 'F') {
+        //     return {
+        //         statusCode : 200,
+        //         body : JSON.stringify(movies)
+        //     };
+        // } else if (comp === 'T' || typeof comp === 'undefined') {
+        //     return {
+        //         statusCode : 200,
+        //         body : JSON.stringify(compress(movies))
+        //     };
+        // } else {
+        //     return {
+        //         statusCode : 404,
+        //         body : 'Invalid query string.'
+        //     };
+        // }
+    } catch (err) {
+        console.error(err);
         return {
-            statusCode : 200,
-            body : JSON.stringify(movies),
-        };
-    } else {
-        const compressed = compress(movies);
-        return {
-            statusCode : 200,
-            body : JSON.stringify(compressed),
+            statusCode : 500,
+            body : JSON.stringify({ message : err instanceof Error ? err.message : err }),
         };
     }
 };
