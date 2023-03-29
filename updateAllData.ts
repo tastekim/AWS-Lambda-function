19ff610bb14eccd6e023357a27c30944eb186790
tastekim <tastekim@kakao.com>
2023-03-29 11:55:26 +0900

    :sparkles: updateAllData.ts 추가

---
+++ updateAllData.ts
@@ -0,0 +1,21 @@
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { getSeasonImg, setDiseasesData_topImg } from './mongodb-modules';
+import { allContentsChange } from './gcs-modules';
+
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    try {
+        await allContentsChange();
+        await setDiseasesData_topImg();
+
+        return {
+            statusCode : 200,
+            body : JSON.stringify({ message: 'Success process.' }),
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode : 500,
+            body : JSON.stringify({ message : err instanceof Error ? err.message : err }),
+        };
+    }
+};
